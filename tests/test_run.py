44a141968d7313ded91cea98bff273f24a53de77
doerp <me@doerp.cool>
2024-04-27 19:37:17 +0200

    skip full test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pytest
 
 from lablib import operators, processors, renderers, utils
 
@@ -19,70 +20,73 @@ STAGING_DIR = "results"
 OUTPUT_WIDTH = 1920
 OUTPUT_HEIGHT = 1080
 
-# Env Setup
-script_location = os.path.dirname(os.path.realpath(__file__))
-os.environ["PATH"] += os.pathsep + os.path.join(script_location, FFMPEG_PATH)
-os.environ["PATH"] += os.pathsep + os.path.join(script_location, OIIO_PATH)
-os.environ["OCIO"] = OCIO_PATH
 
-# Get data from Asset
-with open(DATA_PATH, "r") as f:
-    working_data = json.loads(f.read())
+@pytest.mark.skip(reason="Test is not implemented yet")
+def test_full():
+    # Env Setup
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    os.environ["PATH"] += os.pathsep + os.path.join(script_location, FFMPEG_PATH)
+    os.environ["PATH"] += os.pathsep + os.path.join(script_location, OIIO_PATH)
+    os.environ["OCIO"] = OCIO_PATH
 
-# Setup SequenceInfo operator
-main_seq = operators.SequenceInfo().compute_longest(SOURCE_DIR)
-print(f"{main_seq = }")
+    # Get data from Asset
+    with open(DATA_PATH, "r") as f:
+        working_data = json.loads(f.read())
 
-# Read image info from first image in sequence
-main_seq_info = utils.read_image_info(main_seq.frames[0])
+    # Setup SequenceInfo operator
+    main_seq = operators.SequenceInfo().compute_longest(SOURCE_DIR)
+    print(f"{main_seq = }")
 
-# Compute Effects file from AYON
-epr = processors.EffectsFileProcessor(EFFECT_PATH)
+    # Read image info from first image in sequence
+    main_seq_info = utils.read_image_info(main_seq.frames[0])
 
-# Compute color transformations
-cpr = processors.ColorProcessor(
-    operators=epr.color_operators,
-    config_path=OCIO_PATH,
-    staging_dir=STAGING_DIR,
-    context=working_data["asset"],
-    family=working_data["project"]["code"],
-    views=["sRGB", "Rec.709", "Log", "Raw"],
-)
+    # Compute Effects file from AYON
+    epr = processors.EffectsFileProcessor(EFFECT_PATH)
 
-# Compute repo transformations
-rpr = processors.RepoProcessor(
-    operators=epr.repo_operators,
-    source_width=main_seq_info.display_width,
-    source_height=main_seq_info.display_height,
-    dest_width=OUTPUT_WIDTH,
-    dest_height=OUTPUT_HEIGHT,
-)
+    # Compute color transformations
+    cpr = processors.ColorProcessor(
+        operators=epr.color_operators,
+        config_path=OCIO_PATH,
+        staging_dir=STAGING_DIR,
+        context=working_data["asset"],
+        family=working_data["project"]["code"],
+        views=["sRGB", "Rec.709", "Log", "Raw"],
+    )
 
-# Render the sequence
-rend = renderers.DefaultRenderer(
-    color_proc=cpr,
-    repo_proc=rpr,
-    source_sequence=main_seq,
-    staging_dir=STAGING_DIR,
-    format=".png",
-)
-rend.set_debug(True)
-rend.set_threads(8)
-computed_seq = rend.render()
+    # Compute repo transformations
+    rpr = processors.RepoProcessor(
+        operators=epr.repo_operators,
+        source_width=main_seq_info.display_width,
+        source_height=main_seq_info.display_height,
+        dest_width=OUTPUT_WIDTH,
+        dest_height=OUTPUT_HEIGHT,
+    )
 
-# Compute slate
-spr = processors.SlateProcessor(
-    data=working_data,
-    width=OUTPUT_WIDTH,
-    height=OUTPUT_HEIGHT,
-    staging_dir=STAGING_DIR,
-    slate_template_path=SLATE_TEMPLATE_PATH,
-    is_source_linear=False,
-)
+    # Render the sequence
+    rend = renderers.DefaultRenderer(
+        color_proc=cpr,
+        repo_proc=rpr,
+        source_sequence=main_seq,
+        staging_dir=STAGING_DIR,
+        format=".png",
+    )
+    rend.set_debug(True)
+    rend.set_threads(8)
+    computed_seq = rend.render()
 
-# Render the slate
-rend_slate = renderers.DefaultSlateRenderer(
-    slate_proc=spr, source_sequence=computed_seq
-)
-# rend_slate.set_debug(True)
-rend_slate.render()
+    # Compute slate
+    spr = processors.SlateProcessor(
+        data=working_data,
+        width=OUTPUT_WIDTH,
+        height=OUTPUT_HEIGHT,
+        staging_dir=STAGING_DIR,
+        slate_template_path=SLATE_TEMPLATE_PATH,
+        is_source_linear=False,
+    )
+
+    # Render the slate
+    rend_slate = renderers.DefaultSlateRenderer(
+        slate_proc=spr, source_sequence=computed_seq
+    )
+    # rend_slate.set_debug(True)
+    rend_slate.render()
