932053d066d0791516a4863a847ac7e9b1ed3db8
doerp <me@doerp.cool>
2024-04-27 23:40:38 +0200

    moah

--- lablib/operators/image_info.py
+++ lablib/operators/image_info.py
@@ -11,18 +11,45 @@ log = get_logger(__name__)
 
 @dataclass
 class ImageInfo:
-    file_path: Path = None
+    filepath: Path = None
     filename: str = None
     origin_x: int = 0
     origin_y: int = 0
     width: int = 1920
     height: int = 1080
-    display_width: int = 1920
-    display_height: int = 1080
+    display_width: int = width
+    display_height: int = height
     channels: int = 3
     fps: float = 24.0
     par: float = 1.0
     timecode: str = "01:00:00:01"
+
+    def __init__(self, path: str | Path):
+        super().__init__()
+
+        if isinstance(path, str):
+            path = Path(path)
+
+        if not path.exists():
+            raise FileNotFoundError(f"File not found: {path}")
+
+        if path.suffix not in (".exr"):
+            raise ValueError(f"Invalid file type: {path}")
+
+        self.filepath = path
+        self.filename = path.name
+
+        self.update_from_file()
+
+    def __getitem__(self, k: str) -> any:
+        return getattr(self, k)
+
+    def __setitem__(self, k: str, v: any) -> None:
+        if hasattr(self, k):
+            log.debug(f"Setting {k} to {v}")
+            setattr(self, k, v)
+        else:
+            log.error(f"Cannot set {k}={v}. Key {k} not found.", stack_info=True)
 
     def __run_command(self, cmd: list[str]) -> list[str]:
         log.debug(f"Running command with : {cmd}")
@@ -36,8 +63,8 @@ class ImageInfo:
 
         return result
 
-    def update_from_ffprobe(self) -> None:
-        abspath = str(self.file_path.resolve())
+    def __call_ffprobe(self) -> None:
+        abspath = str(self.filepath.resolve())
         cmd = [
             "ffprobe",
             "-v",
@@ -50,71 +77,79 @@ class ImageInfo:
             "default=noprint_wrappers=1",
             abspath,
         ]
+        result = {}
         for line in self.__run_command(cmd):
-            log.debug(f"{line = }")
             vars = line.split("=")
             if "width" in vars[0]:
-                self.display_width = int(vars[1].strip())
+                result["display_width"] = int(vars[1].strip())
             if "height" in vars[0]:
-                self.display_height = int(vars[1].strip())
+                result["display_height"] = int(vars[1].strip())
             if "r_frame_rate" in vars[0]:
                 rate = vars[1].split("/")
-                self.fps = float(
+                result["fps"] = float(
                     round(float(int(rate[0].strip()) / int(rate[1].strip())), 3)
                 )
             if "timecode" in line:
-                self.timecode = vars[1]
+                result["timecode"] = vars[1]
             if "sample_aspect_ratio" in line:
                 par = vars[1].split(":")
                 if vars[1] != "N/A":
-                    self.par = float(int(par[0].strip()) / int(par[1].strip()))
+                    result["par"] = float(int(par[0].strip()) / int(par[1].strip()))
                 else:
-                    self.par = 1
+                    result["par"] = 1
 
-    def update_from_iinfo(self) -> None:
-        abspath = str(self.file_path.resolve())
+        return result
+
+    def __call_iinfo(self) -> None:
+        abspath = str(self.filepath.resolve())
         cmd = ["iinfo", "-v", abspath]
+        result = {}
         for line in self.__run_command(cmd):
-            log.debug(f"{line = }")
             if abspath in line and line.find(abspath) < 2:
                 vars = line.split(": ")[1].split(",")
                 size = vars[0].strip().split("x")
                 channels = vars[1].strip().split(" ")
-                self.width = int(size[0].strip())
-                self.height = int(size[1].strip())
-                self.display_width = int(size[0].strip())
-                self.display_height = int(size[1].strip())
-                self.channels = int(channels[0].strip())
+                result["width"] = int(size[0].strip())
+                result["height"] = int(size[1].strip())
+                result["display_width"] = int(size[0].strip())
+                result["display_height"] = int(size[1].strip())
+                result["channels"] = int(channels[0].strip())
             if "FramesPerSecond" in line or "framesPerSecond" in line:
                 vars = line.split(": ")[1].strip().split(" ")[0].split("/")
-                self.fps = float(round(float(int(vars[0]) / int(vars[1])), 3))
+                result["fps"] = float(round(float(int(vars[0]) / int(vars[1])), 3))
             if "full/display size" in line:
                 size = line.split(": ")[1].split("x")
-                self.display_width = int(size[0].strip())
-                self.display_height = int(size[1].strip())
+                result["display_width"] = int(size[0].strip())
+                result["display_height"] = int(size[1].strip())
             if "pixel data origin" in line:
                 origin = line.split(": ")[1].strip().split(",")
-                self.origin_x = (int(origin[0].replace("x=", "").strip()),)
-                self.origin_y = (int(origin[1].replace("y=", "").strip()),)
+                result["origin_x"] = (int(origin[0].replace("x=", "").strip()),)
+                result["origin_y"] = (int(origin[1].replace("y=", "").strip()),)
             if "smpte:TimeCode" in line:
-                self.timecode = line.split(": ")[1].strip()
+                result["timecode"] = line.split(": ")[1].strip()
             if "PixelAspectRatio" in line:
-                self.par = float(line.split(": ")[1].strip())
-
-    @classmethod
-    def from_path(cls, path: str | Path) -> ImageInfo:
-        if isinstance(path, str):
-            path = Path(path)
-
-        if not path.exists():
-            raise FileNotFoundError(f"File not found: {path}")
-
-        if path.suffix not in (".exr"):
-            raise ValueError(f"Invalid file type: {path}")
-
-        result = ImageInfo(file_path=path)
-        result.update_from_iinfo()
-        result.update_from_ffprobe()
-        log.info(f"{result = }")
+                result["par"] = float(line.split(": ")[1].strip())
 
         return result
+
+    def update_from_file(self, force_ffprobe=True) -> None:
+        iinfo_res = self.__call_iinfo()
+        log.debug(f"{iinfo_res = }")
+        for k, v in iinfo_res.items():
+            if not v:
+                continue
+            self[k] = v
+
+        ffprobe_res = self.__call_ffprobe()
+        log.debug(f"{ffprobe_res = }")
+        for k, v in ffprobe_res.items():
+            if not v:
+                continue
+            if self[k] != v:
+                if force_ffprobe:
+                    log.debug(f"Updating {k} from {self[k]} to {v}")
+                    self[k] = v
+                else:
+                    log.warning(
+                        f"Values do not match: {k} - {self[k]} != {v} - {ffprobe_res[k]}"
+                    )
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,14 +1,24 @@
 import pytest
+from pathlib import Path
 
 from lablib.operators import ImageInfo
+from lablib.utils import get_logger
+
+log = get_logger(__name__)
 
 
 @pytest.mark.parametrize(
     "path", ["resources/public/plateMain/v000/BLD_010_0010_plateMain_v000.1001.exr"]
 )
 def test_ImageInfo(path: str, caplog):
-    image_info = ImageInfo.from_path(path)
+    image_info = ImageInfo(path)
+    path = Path(path)
+    log.info(f"{image_info = }")
     assert image_info.width == 4382
     assert image_info.height == 2310
     assert image_info.timecode == "02:10:04:17"
     assert image_info.fps == 25.0
+    assert image_info.channels == 3
+    assert image_info.par == 1.0
+    assert image_info.filename == path.name
+    assert image_info.filepath == path
